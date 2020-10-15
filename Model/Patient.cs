eeb4fd7d1134004f00ab0d66a258e0615c914514
AndelikaAdryjanek <y0s1@nykredit.dk>
2020-10-16 00:19:19 +0200

    Add project files.

---
+++ Model/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DP.Patients.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime CovidTestDate { get; set; }
+    }
+}
